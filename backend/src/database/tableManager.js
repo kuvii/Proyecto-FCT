4ae05b5a48d5152f7c4e16c1b70f5786ac278884
kuvii <kuvilet@gmail.com>
2023-04-21 22:36:51 +0100

    [feat] create movement model

--- backend/src/database/tableManager.js
+++ backend/src/database/tableManager.js
@@ -2,12 +2,14 @@ import AccountModel from "../models/Account.js";
 import UserModel from "../models/User.js";
 import CustomerModel from "../models/Customer.js";
 import CardModel from "../models/Card.js";
+import MovementModel from "../models/Movement.js";
 
 const initManager = sequelize => {
     const customer = sequelize.define('Customer', CustomerModel)
     const user = sequelize.define('User', UserModel)
     const account = sequelize.define('Account', AccountModel)
     const card = sequelize.define('Card', CardModel)
+    const movement = sequelize.define('Movement', MovementModel)
 
     customer.hasOne(user)
     user.belongsTo(customer)
@@ -17,6 +19,9 @@ const initManager = sequelize => {
 
     account.hasMany(card)
     card.belongsTo(account)
+
+    account.hasMany(movement)
+    movement.belongsTo(account)
 }
 
 export default initManager
