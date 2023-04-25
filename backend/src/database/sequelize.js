89e337f0aefd0c98abc8579015a2c439a98cd699
kuvii <kuvilet@gmail.com>
2023-04-25 18:18:03 +0100

    [refactor] add logging (used for fix of eb#1)

--- backend/src/database/sequelize.js
+++ backend/src/database/sequelize.js
@@ -3,7 +3,8 @@ import env from '../config/config.js'
 
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASW, {
     host: env.DB_HOST ,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: process.env.NODE_ENV = 'development' ?  console.log: ''
 })
 
 export default sequelize
