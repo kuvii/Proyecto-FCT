218566ad8004eadc12cd208a95d51dc8e5e62a6e
kuvii <kuvilet@gmail.com>
2023-04-27 19:14:19 +0100

    [refactor] add account relations

--- backend/src/models/Account.js
+++ backend/src/models/Account.js
@@ -1,5 +1,10 @@
 import { DataTypes, Model } from "sequelize"
 import sequelize from "../database/sequelize.js"
+import Card from "./Card.js"
+import Loan from "./Loan.js"
+import LoanRequest from './LoanRequest.js'
+import Movement from './Movement.js'
+
 
 class Account extends Model {}
 
@@ -11,6 +16,11 @@ Account.init({
     iban: {
         type: DataTypes.STRING(24)
     }
-}, {sequelize, modelName: 'account'})
+}, {sequelize, modelName: 'account', timestamps: false})
+
+Account.Card = Account.hasMany(Card)
+Account.Loan = Account.hasMany(Loan)
+Account.LoanRequest = Account.hasMany(LoanRequest)
+Account.Movement = Account.hasMany(Movement)
 
 export default Account
