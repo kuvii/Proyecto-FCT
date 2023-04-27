eb46c6dd3db94062fc6fc3fa380c3aa75302558b
kuvii <kuvilet@gmail.com>
2023-04-27 19:16:38 +0100

    [feat] add encryption to password, and user auth

--- backend/src/services/customerServices.js
+++ backend/src/services/customerServices.js
@@ -1,6 +1,6 @@
 import Customer from "../models/Customer.js"
 import User from "../models/User.js"
-import Account from "../models/Account.js"
+import bcrypt from 'bcrypt'
 
 const findOneCustomer = async (id) => {
     const customer = await Customer.findAll({
@@ -16,12 +16,15 @@ const findOneCustomer = async (id) => {
 }
 
 const createNewCustomer = async (customerData) => {
+
+    const salt = await bcrypt.genSalt(10)
+
     try {
         await Customer.create({
             first_name: customerData?.first_name,
             last_name: customerData?.last_name,
             birthdate: customerData?.birthdate,
-            password: customerData?.password,
+            password: await bcrypt.hash(customerData?.password, salt),
             email: customerData?.email,
             dni: customerData?.dni,
             phone: customerData?.phone,
@@ -45,7 +48,29 @@ const createNewCustomer = async (customerData) => {
     }
 }
 
+const authUser = async (customerToAuth) => {
+    try {
+        // Use this function in {find one customer}
+        const foundUser = await Customer.findOne({
+            where: {
+                email: customerToAuth.email
+            }
+        })
+        // -------
+        if (foundUser){
+            const password_valid = await bcrypt.compare(customerToAuth.password, foundUser.password)
+            if (password_valid){
+                return true
+            }
+        }
+        return false
+    } catch (error) {
+        throw new Error (error)
+    }
+}
+
 export const customerServices = {
     findOneCustomer,
-    createNewCustomer
+    createNewCustomer,
+    authUser
 }
