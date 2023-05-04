b5279fd09b8f41f92f0d5cf117885d992a6262e7
ElCeacker <gabrigabriel1010@gmail.com>
2023-05-04 11:39:27 +0100

    [refactor] stored username and password

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,9 +1,26 @@
 import imagenCandado from '../../assets/candado.png'
 import imagenUsuario from '../../assets/usuario.png'
 import imagenLogo1 from '../../assets/KingsBank_BlancoVerde1.png'
+import { useState } from 'react'
 import SliderPhotos from '../slider/SliderPhotos'
 
 const Login = () => {
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (user === "" || password === "") {
+            alert("Tiene que rellenar todos los campos para poder registrarse")
+        } else {
+            setUser(user);
+            setPassword(password);
+        }
+    };
+
+
     return (
         <>
             <nav className="navbar navbar-expand-lg contenedorNavbar">
@@ -13,12 +30,14 @@ const Login = () => {
                         <span className="titulo">Kings Bank</span>
                     </div>
                     <div className="header">
-                        <form className="d-flex ml-auto formMax">
+                        <form onSubmit={handleSubmit} className="d-flex ml-auto formMax">
                             <div className='containerInputs'>   
                                 <div className="inputContainer1">
                                     <input className="space" 
                                     type="text" 
                                     placeholder="Username" 
+                                    value={user}
+                                    onChange={(e) => setUser(e.target.value)}
                                     />
                                     <span className="inputLogo"><img src={imagenUsuario} className='imagenLogo' alt='Imagen del Logo Usuario'/></span>
                                 </div>
@@ -27,6 +46,8 @@ const Login = () => {
                                     <input className="space" 
                                     type="text" 
                                     placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                     />
                                     <span className="inputLogo"><img src={imagenCandado} className='imagenLogo' alt='Imagen del Logo Contraseña'/></span>
                                 </div>
@@ -47,6 +68,8 @@ const Login = () => {
                 <span className="footerText">KingsBank &copy; 2023</span>
             </div>
         </>
+
+        
     )
 }
 
