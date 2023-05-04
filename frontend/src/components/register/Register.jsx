27a85929afd3d4896da3c8d83bfc7bfc4df30b61
ElCeacker <gabrigabriel1010@gmail.com>
2023-05-04 11:59:04 +0100

    [feat] record header done

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import imagenLogo1 from '../../assets/KingsBank_BlancoVerde1.png'
 
 const Register = () => {
+
     return (
-        <div>Register</div>
+        <>
+            <nav className="navbar navbar-expand-lg contenedorNavbar">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <div className="logoTitulo">
+                        <img src={imagenLogo1} className='imagenKingBank' alt='Imagen del Logo'/>
+                        <span className="titulo">Kings Bank</span>
+                    </div>
+                    <div className="header">
+                        <form className="d-flex ml-auto formMax">
+                            <div className='containerRegisterLogin'>
+                                <h2 className='titulo'>Registro</h2>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="footer">
+                <span className="footerText">KingsBank &copy; 2023</span>
+            </div>
+        </>
+
+        
     )
 }
 
