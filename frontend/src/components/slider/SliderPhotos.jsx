7ebf782deb9c8b3ad49ae87aaaa08146c14813c4
ElCeacker <gabrigabriel1010@gmail.com>
2023-05-04 11:14:01 +0100

    [feat] component (SliderPhotos) added

---
+++ frontend/src/components/slider/SliderPhotos.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import {
+Carousel,
+CarouselItem,
+CarouselControl,
+CarouselIndicators,
+CarouselCaption
+} from 'reactstrap';
+
+import imagen1 from '../../assets/imagen1.png'
+import imagen2 from '../../assets/imagen2.jpg'
+import imagen3 from '../../assets/imagen3.png'
+
+const items = [
+{
+    src: imagen1,
+    altText: 'Siria',
+    caption: 'Siria necesita tu ayuda'
+},
+{
+    src: imagen2,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+},
+{
+    src: imagen3,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+}
+];
+
+class SliderPhotos extends Component {
+constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+}
+
+onExiting() {
+    this.animating = true;
+}
+
+onExited() {
+    this.animating = false;
+}
+
+next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+}
+
+previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+}
+
+goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+}
+
+render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+    return (
+        <CarouselItem
+        onExiting={this.onExiting}
+        onExited={this.onExited}
+        key={item.src}
+        >
+        <img src={item.src} alt={item.altText} className='anuncios'/>
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+    );
+    });
+
+    return (
+    <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+    >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous}/>
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+    </Carousel>
+    );
+}
+}
+
+
+export default SliderPhotos;
