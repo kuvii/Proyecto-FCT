91e234b1f7442cd34abc7a0e98c17344723347ef
kuvii <kuvilet@gmail.com>
2023-05-05 22:17:38 +0100

    [refactor] add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 // import Login from './components/login/Login';
 import Register from './components/register/Register';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       {/* <Login/> */}
       <Register/>
-    </div>
+    </BrowserRouter>
   );
 }
 
