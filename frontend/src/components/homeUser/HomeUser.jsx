7cf6abcf77f8cb279d650636fa5815916aca88a5
Practicas <rtpracticas@INTERNO.CRI>
2023-05-08 13:36:09 +0100

    [style] changed visual styles on both mobile and desktop browsers

--- frontend/src/components/homeUser/HomeUser.jsx
+++ frontend/src/components/homeUser/HomeUser.jsx
@@ -2,41 +2,61 @@ import imagenLogo1 from '../../assets/KingsBank_BlancoVerde1.png'
 
 const HomeUser = () => {
     return (
-        <>
+        <div className='containerPage'>
             <nav className="navbar navbar-expand-lg contenedorNavbar mainContainer">
                 <div className="collapse navbar-collapse headerContainer" id="navbarTogglerDemo01">
                     <img src={imagenLogo1} className='imagenKingBank' alt='Imagen del Logo'/>
                     <div className="header">
-                        <div className='contenedorTitulo'>
-                            <h2 className='title'>Welcome back</h2>
-                        </div>
+                        <h2 className='title'>Welcome back</h2>
                     </div>
                 </div>
             </nav>
-
-            <div className='containerDataCard'>
-                <span>Total</span>
-                <div className='card'>
-                    <div className='subContainerMoney'>
-                        <span>dinero€</span>
-                    </div>
-                    <div className='subContainerES'>
-                        <span>número de cuenta ES</span>
-                    </div>
-                </div>
-                <span>Tarjetas</span>
-                <div className='card2'>
-                    <div className='subcontainerCards'>
-                        <div className='prueba'>
-                            cuadrado1
+            <div className='containerCardAndLastMovements'>
+                <div className='containerDataCard'>
+                    <span>Total</span>
+                    <div className='card'>
+                        <div className='subContainerMoney'>
+                            <span>dinero€</span>
+                        </div>
+                        <div className='subContainerES'>
+                            <span>número de cuenta ES</span>
                         </div>
                     </div>
-                    <div className='subContainerRequestCard'>
-                        cuadrado2
+                    <span>Tarjetas</span>
+                    <div className='card2'>
+                        <div className='subcontainerCards'>
+                            <div className='prueba'>
+                                cuadrado1
+                            </div>
+                        </div>
+                        <div className='subContainerRequestCard'>
+                            cuadrado2
+                        </div>
                     </div>
                 </div>
+                <div className='lastMovements'>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                    <p>Contenido dentro del ScrollView</p>
+                </div>
+                <span></span>
             </div>
-        </>
+        </div>
     )
 }
 
