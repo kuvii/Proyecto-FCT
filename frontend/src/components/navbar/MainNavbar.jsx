cecfb16c65f2e96cea5ab422634d51fd1b9b79e5
kuvii <kuvilet@gmail.com>
2023-05-08 22:17:13 +0100

    [refactor] simplified code by exporting it

--- frontend/src/components/navbar/MainNavbar.jsx
+++ frontend/src/components/navbar/MainNavbar.jsx
@@ -1,81 +1,10 @@
 import React, { useState } from 'react'
-import { Button, Col, Form, FormGroup, Nav, Navbar, NavbarBrand, NavbarToggler, Row, Collapse } from 'reactstrap'
-import { Alert, Stack, TextField, ThemeProvider, createTheme, useMediaQuery, useTheme } from '@mui/material'
+import { Navbar, NavbarBrand, NavbarToggler, } from 'reactstrap'
+import { Collapse, Container, ThemeProvider, createTheme, useMediaQuery, useTheme, } from '@mui/material'
 import logo from '../../assets/KingsBank_BlancoVerde1.png'
-
-const initLoginAuthorizationBody = {
-  email: '',
-  password: ''
-}
-
-const EmailLoginInput = ({value, onChange}) => {
-  return (
-    <Col>
-      <FormGroup floating>
-        <TextField autoComplete='off' id='userEmailLogin' name='email' label='Email' type='email' variant='standard' color='neutral' value={value} onChange={onChange} /> 
-      </FormGroup>
-    </Col>
-  )
-}
-
-const PasswordLoginInput = ({value, onChange}) => {
-  return (
-    <Col>
-      <FormGroup floating>
-        <TextField autoComplete='off' id='userPasswordLogin' name='password' label='Password' type='text' variant='standard' color='neutral' value={value} onChange={onChange}/> 
-      </FormGroup>
-    </Col>
-  )
-}
-
-const ButtonLogin = () => {
-  return (
-    <Col>
-      <Button type="submit">
-        Login
-      </Button>
-    </Col>
-  )
-}
-
-const CollapsedForm = (props) => {
-  return (
-    <>
-      <NavbarToggler onClick={props.toggleNavBar}/>
-      <Collapse isOpen={props.isOpen} navbar>
-        <Nav navbar>
-          <LoginForm handleChange={props.handleChange} handleSubmit={props.handleSubmit} loginAuthorizationBody={props.loginAuthorizationBody}/>
-        </Nav>
-      </Collapse>
-    </>
-  )
-}
-
-const LoginForm = (props) => {
-  return (
-    <Form onSubmit={props.handleSubmit}>
-      <Row className="row-cols-lg-auto g-3 align-items-center">
-        <Stack 
-        direction={{xs: 'column', sm: 'row'}}
-        spacing={{xs: 1, sm: 2, md: 2}}
-        useFlexGap
-        flexWrap='wrap'
-        >
-          <EmailLoginInput value={props.loginAuthorizationBody.email} onChange={(e) => props.handleChange(e)} />
-          <PasswordLoginInput value={props.loginAuthorizationBody.password} onChange={(e) => props.handleChange(e)}/>
-          <ButtonLogin />
-        </Stack>
-      </Row>
-    </Form>
-  )
-}
+import Login from '../login/Login'
 
 const MainNavbar = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
-  const [loginAuthorizationBody, setLoginAuthorizationBody] = useState(initLoginAuthorizationBody)
-
-  const toggleNavBar = () => setIsOpen(!isOpen)
 
   const defaultTheme = useTheme()
   const widthControlTrigger = useMediaQuery(defaultTheme.breakpoints.up('sm'))
@@ -93,31 +22,21 @@ const MainNavbar = () => {
       },
   })
 
-  const validateEmail = (email) => {
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-    if (email.match(emailRegex)){
-      return true
-    }
-    return false
-  }
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleNavBar = () => setIsOpen(!isOpen)
 
-  const handleChange = (e) => {
-    setLoginAuthorizationBody(prevState => ({
-      ...prevState,
-      [e.target.name]: e.target.value
-    }))
-  }
+  const CollapsedNavbar = ({isOpen}) => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const { email, password } = loginAuthorizationBody
-
-    if (email === '' || password === ''){
-      <Alert severity='error'>Por favor rellena ambos campos</Alert>
-    }
-    if (!validateEmail(email)){
-      <Alert severity='error'>Email incorrecto</Alert>
-    }
+    return (
+      <>
+        <NavbarToggler onClick={toggleNavBar}/>
+        <Container maxWidth='sm'>
+          <Collapse in={isOpen} >
+              <Login/>
+          </Collapse>
+        </Container>
+      </>
+    )
   }
 
   return (
@@ -128,8 +47,7 @@ const MainNavbar = () => {
           KingsBank
         </NavbarBrand>
         <ThemeProvider theme={theme}>
-          {widthControlTrigger ? <LoginForm handleChange={handleChange} handleSubmit={handleSubmit} loginAuthorizationBody={loginAuthorizationBody} /> 
-          : <CollapsedForm toggleNavBar={toggleNavBar} handleChange={handleChange} handleSubmit={handleSubmit} loginAuthorizationBody={loginAuthorizationBody} />}
+            {widthControlTrigger ? <Login/> : <CollapsedNavbar isOpen={isOpen} />}
         </ThemeProvider>
     </Navbar>
     </div>
