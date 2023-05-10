b161d515133c35ab65c26567b375c10d2b9f5e14
kuvii <kuvilet@gmail.com>
2023-05-10 21:49:21 +0100

    [feat] secured private routes

--- frontend/src/app/KingsbankApp.jsx
+++ frontend/src/app/KingsbankApp.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../screens/home/Home'
 import Body from '../layout/body/Body'
 
 const KingsbankApp = () => {
+
+  function RequireAuth ({ children }) {
+    return localStorage.getItem("user") !== null ? children : <Navigate to="/" replace/>
+  }
+
   return (
     <div>
       <Routes>
         <Route element={<Body/>}>
           <Route path='/' element={<Home/>}/>
         </Route>
-      
-        <Route path='/my' element={<h1>Dashboard</h1>}/>
+
+        <Route path='/my' 
+        element={
+            <RequireAuth>
+              <h1>Dashboard</h1>
+            </RequireAuth>
+          }
+        />
       </Routes>
     </div>
   )
