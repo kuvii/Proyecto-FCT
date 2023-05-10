16eb87492ffc851a4d2481bbe0db8fd4374c7c9c
kuvii <kuvilet@gmail.com>
2023-05-10 20:40:59 +0100

    [refactor] remove navbar on outlet routes

--- frontend/src/app/KingsbankApp.jsx
+++ frontend/src/app/KingsbankApp.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../screens/home/Home'
-import MainNavbar from '../components/navbar/MainNavbar'
+import Body from '../layout/body/Body'
 
 const KingsbankApp = () => {
   return (
     <div>
-      <MainNavbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route element={<Body/>}>
+          <Route path='/' element={<Home/>}/>
+        </Route>
+      
         <Route path='/my' element={<h1>Dashboard</h1>}/>
       </Routes>
     </div>
