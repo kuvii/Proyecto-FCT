4c1e058bcd830458734b9654088e1d8e0479c8b9
kuvii <kuvilet@gmail.com>
2023-05-12 20:17:26 +0100

    [feat] create movement component

---
+++ frontend/src/components/movement/Movement.jsx
@@ -0,0 +1,33 @@
+import { Box, Paper, Stack, Typography } from '@mui/material'
+import React from 'react'
+
+const Movement = (props) => {
+    const { content, style } = props
+    return (
+        <Box>
+            <Paper sx={{margin: 1, padding: 2, ...style}}>
+                <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                >
+                    <Box>
+                        {content?.message || <div>Message not found</div>}
+                    </Box>
+                    <Box>
+                        <Stack>
+                            {content?.quantity || <div>?€</div>}
+                            <Typography variant="caption" display="block" gutterBottom>
+                                {content?.date || <div>xx-xx-xxxx</div>}
+                            </Typography>
+                        </Stack>
+                    </Box>
+                </Stack>
+                <Stack>
+                </Stack>
+            </Paper>
+        </Box>
+    )
+}
+
+export default Movement
