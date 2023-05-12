df77e732915ae1c7bb46cf9ad62afc4639f7e7a3
kuvii <kuvilet@gmail.com>
2023-05-12 22:23:48 +0100

    [feat] add navigation to customer selected

--- frontend/src/components/customer-edit/CustomerEdit.jsx
+++ frontend/src/components/customer-edit/CustomerEdit.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Box, Paper, Stack, useMediaQuery, useTheme } from '@mui/material'
+import { useNavigate } from 'react-router-dom';
 
 const CustomerEdit = (props) => {
     const {content, style } = props
 
-    const theme = useTheme();
+    const theme = useTheme()
+    const navigate = useNavigate()
     const isPc = useMediaQuery(theme.breakpoints.up('sm'));
 
-    const { name, email } = content
+    const { id, name, email } = content
     const shortName = name.substring(0, 7).replace(' ', '') + '...'
     const shortEmail = email.substring(0, 7).replace(' ', '') + '...' 
 
+    const handleClick = (id) => {
+        navigate('/admin/customer/' + id, {state: {content}})
+    }
+
     return (
-        <Box>
+        <Box onClick={() => handleClick(id)} style={{cursor: 'pointer'}}>
             <Paper sx={{margin: 1, padding: 2, backgroundColor: (theme.palette.mode === 'light' ? '#3590E4' : '#000000'), color: '#ffffff', ...style}}>
                 <Stack
                 direction="row"
