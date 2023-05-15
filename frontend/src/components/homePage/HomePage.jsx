d9f7be70f95668d116b758895027aaba43200ab2
Practicas <rtpracticas@INTERNO.CRI>
2023-05-15 10:15:50 +0100

    [feat] finished home component visually

--- frontend/src/components/homePage/HomePage.jsx
+++ frontend/src/components/homePage/HomePage.jsx
@@ -1,7 +1,33 @@
-const homePage = () => {
+import imagenLogo1 from '../../assets/KingsBank_BlancoVerde1.png'
+import imagenUsuario1 from '../../assets/usuario1.png'
+
+const HomePage = () => {
+
     return (
-        <div>homePage</div>
+        <div className='generalContainer'>
+            <div className='containerPage'>
+                <nav className="navbar navbar-expand-lg contenedorNavbar mainContainer">
+                    <div className="collapse navbar-collapse headerContainer" id="navbarTogglerDemo01">
+                        <img src={imagenLogo1} className='imagenKingBank' alt='Imagen del Logo'/>
+                        <div className="header">
+                            <h2 className='title'>Welcome back</h2>
+                        </div>
+                    </div>
+                </nav>
+                
+            </div>
+            <div className='containerPage2'>
+                <img src={imagenUsuario1} alt="usuario1" className='imgUsuario1' />
+                <span className='titleUser'>User</span>
+                <div className='containerUserLinks'>
+                    <a href='/Inicio' className='userLinks'>Inicio</a>
+                    <a href='/' className='userLinks'>Movimientos</a>
+                    <a href='/' className='userLinks'>Tarjetas</a>
+                    <a href='/' className='userLinks'>Préstamos</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default homePage
+export default HomePage
