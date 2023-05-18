c3045cc617272a5bb802eef7fa32eaffcac1ffb9
kuvii <kuvilet@gmail.com>
2023-05-18 20:05:33 +0100

    [refactor] create component dashboard

---
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,34 @@
+import { Box, Grid, useMediaQuery, useTheme } from '@mui/material'
+import React from 'react'
+import Card from '../card/Card'
+import Movement from '../movement/Movement'
+
+const Dashboard = ({ userInfo }) => {
+    const theme = useTheme()
+    const isPc = useMediaQuery(theme.breakpoints.up('sm'))
+    return (
+        <Box component='div' sx={{ overflowY: 'auto', marginRight: (isPc ? '205px' : null) }}>
+        <Grid
+        container
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+            <Grid item xs={4} sm={4} md={6}>
+                <Box margin={1} height='400px' sx={{ overflowY: 'auto'}}>
+                    {userInfo.account.cards && userInfo.account.cards.map((card) => (
+                        <Card cardInfo={card} key={card.id} style={ {marginY: 1} } />
+                    ))}
+                </Box>
+            </Grid>
+            <Grid item xs={4} sm={4} md={6}>
+                <Box height='400px' sx={{ overflowY: 'auto'}}>
+                    {userInfo.account.movements && userInfo.account.movements.map((move) => (
+                        <Movement content={move} key={move.id} style={ {marginY: 1} } />
+                    ))}
+                </Box>
+            </Grid>
+        </Grid>
+    </Box>
+    )
+}
+
+export default Dashboard
