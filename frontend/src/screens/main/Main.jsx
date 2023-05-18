f3a4f008408f10ccdd6b1f7bc02ec1265938fd44
kuvii <kuvilet@gmail.com>
2023-05-18 21:35:06 +0100

    [refactor] add outlet and responsive box

--- frontend/src/screens/main/Main.jsx
+++ frontend/src/screens/main/Main.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react'
 
-import Dashboard from '../../components/dashboard/Dashboard';
+// import Dashboard from '../../components/dashboard/Dashboard
 import MainScreenNavbar from '../../components/main_screen_navbar/MainScreenNavbar';
 import MainDrawer from '../../components/maindrawer/MainDrawer';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { Outlet } from 'react-router';
 
 
 const Main = ({children, userInfo}) => {
     const [open, setOpen] = useState(false)
 
+    const theme = useTheme()
+    const isPc = useMediaQuery(theme.breakpoints.up('sm'))
+
     const handleOpen = () => {
         setOpen(!open)
     }
+
+    const drawerWidth = '200px'
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }} >
         <MainScreenNavbar open={open} handleOpen={handleOpen} />
         <MainDrawer open={open} handleOpen={handleOpen} userInfo={userInfo} />
-            <Dashboard userInfo={userInfo} />
-            {children}
+            <Box maxWidth={isPc ? `calc(100% - ${drawerWidth})` : '100%'}>
+                <Outlet />
+            </Box>
         </Box>
     )
 }
