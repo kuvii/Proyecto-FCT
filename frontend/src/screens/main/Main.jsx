febc9ffab5c3c3d8f2f80f60c55383dbf7572f66
kuvii <kuvilet@gmail.com>
2023-05-18 20:06:35 +0100

    [refactor] removed refactored code

--- frontend/src/screens/main/Main.jsx
+++ frontend/src/screens/main/Main.jsx
@@ -1,80 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import imagenUsuario1 from '../../assets/usuario1.png'
-import { Box, Drawer, Grid, IconButton, useMediaQuery, useTheme } from '@mui/material'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import React, { useState } from 'react'
 
-import MainScreenNavbar from '../../components/main_screen_navbar/MainScreenNavbar'
+import Dashboard from '../../components/dashboard/Dashboard';
+import MainScreenNavbar from '../../components/main_screen_navbar/MainScreenNavbar';
+import MainDrawer from '../../components/maindrawer/MainDrawer';
+import { Box } from '@mui/material';
 
-import LinkList from '../../components/link_list/LinkList'
-import Movement from '../../components/movement/Movement';
-import Card from '../../components/card/Card';
-import { margin } from '@mui/system';
 
-const Main = (props) => {
-    const theme = useTheme()
-    const isPc = useMediaQuery(theme.breakpoints.up('sm'))
+const Main = ({children, userInfo}) => {
     const [open, setOpen] = useState(false)
 
     const handleOpen = () => {
         setOpen(!open)
     }
 
-    const MainDrawer = () => {
-        return (
-            <Drawer variant={isPc ? 'permanent' : 'persistent'} open={open} anchor='right' 
-            PaperProps={ { 
-                    style: { 
-                        borderTopLeftRadius: 20, 
-                        borderBottomLeftRadius: 20, 
-                        backgroundColor: theme.palette.mode === 'light' ? '#ffffff' : '#000000' ,
-                        minWidth: isPc ? 200 : 100,
-                        flex: 1
-                    }
-                } }>
-                    {isPc 
-                        ? ''
-                        : (
-                        <IconButton sx={ {position: 'absolute'} } onClick={handleOpen}>
-                            <ChevronRightIcon/>
-                        </IconButton>
-                        ) 
-                        
-                    }
-                    <Box alignItems='center' justifyContent='center'>
-                        <img src={imagenUsuario1} alt="usuario1" className='imgUsuario1' />
-                    </Box>
-                    <span className='titleUser'>{props.userInfo.first_name}</span>
-                    <LinkList />
-            </Drawer>
-        )
-    }
-
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }} >
-            <MainScreenNavbar open={open} handleOpen={handleOpen} />
-            <MainDrawer />
-            <Box component='div' sx={{ overflowY: 'auto', marginRight: (isPc ? '205px' : null) }}>
-                <Grid
-                container
-                columns={{ xs: 4, sm: 8, md: 12 }}
-                >
-                    <Grid item xs={4} sm={4} md={6}>
-                        <Box margin={1} height='400px' sx={{ overflowY: 'auto'}}>
-                            {props.userInfo.account.cards && props.userInfo.account.cards.map((card) => (
-                                <Card cardInfo={card} key={card.id} style={ {marginY: 1} } />
-                            ))}
-                        </Box>
-                    </Grid>
-                    <Grid item xs={4} sm={4} md={6}>
-                        <Box height='400px' sx={{ overflowY: 'auto'}}>
-                            {props.userInfo.account.movements && props.userInfo.account.movements.map((move) => (
-                                <Movement content={move} key={move.id} style={ {marginY: 1} } />
-                            ))}
-                        </Box>
-                    </Grid>
-                </Grid>
-                
-            </Box>
+        <MainScreenNavbar open={open} handleOpen={handleOpen} />
+        <MainDrawer open={open} handleOpen={handleOpen} userInfo={userInfo} />
+            <Dashboard userInfo={userInfo} />
+            {children}
         </Box>
     )
 }
