f08485169260d9eb84b90f5e313dff2ee484dfb7
kuvii <kuvilet@gmail.com>
2023-05-18 22:05:36 +0100

    [refactor] shorted height on phone view

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,16 @@
-import { Box, Grid, } from '@mui/material'
+import { Box, Grid, useMediaQuery, useTheme, } from '@mui/material'
 import React from 'react'
 import Card from '../card/Card'
 import Movement from '../movement/Movement'
 
 const Dashboard = ({ userInfo }) => {
+
+    const boxHeightPc = '400px'
+    const boxHeightPhone = '300px'
+
+    const theme = useTheme()
+    const isPc = useMediaQuery(theme.breakpoints.up('sm'))
+
     return (
         <Box component='div' sx={{ overflowY: 'auto' }}>
         <Grid
@@ -11,14 +18,14 @@ const Dashboard = ({ userInfo }) => {
         columns={{ xs: 4, sm: 8, md: 12 }}
         >
             <Grid item xs={4} sm={4} md={6}>
-                <Box margin={1} height='400px' sx={{ overflowY: 'auto'}}>
+                <Box marginX={1} height={isPc ? boxHeightPc : boxHeightPhone} sx={{ overflowY: 'auto'}}>
                     {userInfo.account.cards && userInfo.account.cards.map((card) => (
                         <Card cardInfo={card} key={card.id} style={ {marginY: 1} } />
                     ))}
                 </Box>
             </Grid>
             <Grid item xs={4} sm={4} md={6}>
-                <Box height='400px' sx={{ overflowY: 'auto'}}>
+                <Box height={isPc ? boxHeightPc : boxHeightPhone} sx={{ overflowY: 'auto'}}>
                     {userInfo.account.movements && userInfo.account.movements.map((move) => (
                         <Movement content={move} key={move.id} style={ {marginY: 1} } />
                     ))}
