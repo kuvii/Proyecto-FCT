c2b6190971e21647e0e8d02f24318fc0048660f3
kuvii <kuvilet@gmail.com>
2023-05-20 21:17:46 +0100

    [feat] add pages options

--- frontend/src/components/link_list/LinkList.jsx
+++ frontend/src/components/link_list/LinkList.jsx
@@ -2,14 +2,36 @@ import React from "react"
 import { List, ListItem } from "@mui/material"
 import { Link } from "react-router-dom"
 
+const dashboardOptions = {
+    header: 'Inicio'
+}
+
+const movementsOptions = {
+    header: 'Movimientos'
+}
+
+const cardsOptions = {
+    header: 'Tarjetas'
+}
+
+const loansOptions = {
+    header: 'Prestamos'
+}
+
 const LinkList = () => {
+
+    const handleNavigation = (index) => {
+        const optionsList = [dashboardOptions, movementsOptions, cardsOptions, loansOptions]
+        localStorage.setItem('navbar_options', JSON.stringify(optionsList[index]))
+    }
+
     return (
         <List>
             {["Inicio", "Movimientos", "Tarjetas", "Prestamos"].map((text, index) => {
                 const routes = ["/my", "/my/movements", "/my/cards", "/my/loans"]
                 return (
                     <ListItem key={text} disablePadding sx={ { marginY: 5, justifyContent: 'center'} } >
-                        <Link to={routes[index]} className='userLinks' >{text}</Link>
+                        <Link to={routes[index]} className='userLinks' onClick={() => handleNavigation(index)} >{text}</Link>
                     </ListItem>
                 )
             })
