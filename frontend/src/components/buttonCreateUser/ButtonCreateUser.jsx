6512f28170cc8341a6af7bfe239f577f89c7659d
Practicas <rtpracticas@INTERNO.CRI>
2023-05-22 13:31:35 +0100

    [feat] component to create the user ready to use

--- frontend/src/components/buttonCreateUser/ButtonCreateUser.jsx
+++ frontend/src/components/buttonCreateUser/ButtonCreateUser.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import Formulary from "../formulary/Formulary";
 
 const ButtonCreateUser = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <div>ButtonCreateUser</div>
+        <div>
+
+            <button className="btnCreateUser" onClick={openModal}>Crear Usuario</button>
+    
+            {isOpen && (
+                <div className="modal-backdrop">
+                    <Formulary/>
+                    <button onClick={closeModal} className="cancelCreateUser">Cancelar</button>
+                </div>
+            )}
+
+        </div>
     )
 }
 
