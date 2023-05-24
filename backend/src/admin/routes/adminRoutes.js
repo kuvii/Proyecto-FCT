f2fae722c8e350826bf4062d9f648bf3287224d9
kuvii <kuvilet@gmail.com>
2023-05-24 21:47:33 +0100

    [feat] admin loans requests routes

--- backend/src/admin/routes/adminRoutes.js
+++ backend/src/admin/routes/adminRoutes.js
@@ -11,4 +11,8 @@ adminRouter.get('/admin/get-role/:id', adminController.getUserRoleFromId)
 
 adminRouter.put('/admin/cards/requests/:id', adminController.updateCardRequest)
 
+adminRouter.get('/admin/loan-requests', adminController.getLoanRequestsList)
+
+adminRouter.put('/admin/loan-requests/loan/:id', adminController.updateLoan)
+
 export default adminRouter
