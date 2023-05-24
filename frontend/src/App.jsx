a0dd791db74662dffd4b1dc6cd2988b4fa7f4b41
kuvii <kuvilet@gmail.com>
2023-05-24 20:22:36 +0100

    [refactor] delete unused import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css';
-// import KingsbankApp from './app/KingsbankApp';
-// import { BrowserRouter } from 'react-router-dom'
-// import ButtonCreateUser from './components/buttonCreateUser/ButtonCreateUser';
-
-import HomePage from './components/homePage/HomePage'
+import KingsbankApp from './app/KingsbankApp';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <KingsbankApp/>
-    // </BrowserRouter>
-    <HomePage/>
+    <BrowserRouter>
+      <KingsbankApp/>
+    </BrowserRouter>
   );
 }
 
