fce29662858bfde0a9e772e7ce04ba26b22a6e09
kuvii <kuvilet@gmail.com>
2023-05-25 23:00:48 +0100

    [feat] add empty movement box

--- frontend/src/components/customer-components/MovementsList.jsx
+++ frontend/src/components/customer-components/MovementsList.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Movement from '../movement/Movement'
 import apiCustomer from '../../api/customer'
-import { Alert, Snackbar } from '@mui/material'
+import { Alert, Box, Snackbar, useTheme } from '@mui/material'
+import themeHandler from '../../utils/theme'
 
 const MovementsList = () => {
+
+    const theme = useTheme()
 
     const [movementList, setMovementList] = useState([])
     const [openErrorSnackbar, setOpenErrorSnackbar] = useState(false)
@@ -18,13 +21,32 @@ const MovementsList = () => {
             }
         }
         fetchMovements()
-    })
+    }, [])
     
     return (
         <div>
-            {movementList && movementList.map((move) => (
-                <Movement content={move} key={move.id} style={ {marginY: 1} } />
-            ))}
+            {
+                movementList === 0 ? (
+                    <Box sx={{backgroundColor: theme.palette.mode === 'light' ? 
+                              themeHandler.LIGHT_MODE.secondary_color : 
+                              themeHandler.DARK_MODE.secondary_color,
+                            }}
+                        height={50}
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='center'
+                        borderRadius={2}
+                    >
+                        No hay movimientos
+                    </Box>
+                ) : (
+                    <>
+                    {movementList && movementList.map((move) => (
+                        <Movement content={move} key={move.id} style={ {marginY: 1} } />
+                    ))}
+                    </>
+                )
+            }
             <Snackbar open={openErrorSnackbar} anchorOrigin={{vertical: 'top', horizontal: 'left'}} autoHideDuration={3000} onClose={() => setOpenErrorSnackbar(false)}>
                 <Alert variant='filled' onClose={() => setOpenErrorSnackbar(false)} severity="error" sx={{ width: '100%' }}>
                     No se ha podido cargar los movimientos
