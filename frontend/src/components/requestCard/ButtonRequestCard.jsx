d0052309df8cd783bbaf26a771cd1f5b4a291805
ElCeacker <gabrigabriel1010.com>
2023-05-28 21:20:42 +0100

    [feat] request card button created with your selections implemented and saved

--- frontend/src/components/requestCard/ButtonRequestCard.jsx
+++ frontend/src/components/requestCard/ButtonRequestCard.jsx
@@ -1,8 +1,39 @@
+import { Button } from '@mui/material'
+import { useState } from 'react';
 
 const ButtonRequestCard = () => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [select, setSelect] = useState(''); // Aquí se queda guardada la opcion escogida.
+
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
+    const saveOption = (option) => {
+        setSelect(option);
+        closeModal();
+    };
+    
     return (
-        <div>ButtonRequestCard</div>
-    )
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Button variant='contained' size='small' onClick={openModal} style={{width: '100px'}}>Nuevo</Button>
+    
+            {showModal && (
+            <div className="modal">
+                <div className="modal-content">
+                    <h2>Selecciona una opción</h2>
+                    <button onClick={() => saveOption('Débito')} className='selectionCard'>Débito</button>
+                    <button onClick={() => saveOption('Crédito')} className='selectionCard'>Crédito</button>
+                </div>
+            </div>
+            )}
+        </div>
+    );
 }
 
 export default ButtonRequestCard
