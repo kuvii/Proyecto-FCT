048e6c83ac6e137cf1c7b0c558c2c3e31b7066c2
kuvii <kuvilet@gmail.com>
2023-05-29 20:18:07 +0100

    [feat] add component if requests are empty

--- frontend/src/screens/admin/LoansPage.jsx
+++ frontend/src/screens/admin/LoansPage.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import apiAdmin from '../../api/admin'
 import Loan from '../../components/loan/Loan'
 import { Alert, Box, Snackbar } from '@mui/material'
+import { ColorModeContext } from '../../app/KingsbankApp'
+import themeHandler from '../../utils/theme'
 
 const LoansPage = () => {
     const [loanList, setLoanList] = useState([])
+    const { mode } = useContext(ColorModeContext);
     
     useEffect(() => {
         const loadLoanList = async () => {
@@ -30,14 +33,35 @@ const LoansPage = () => {
     }
 
     return (
-        <Box>{loanList && loanList.map((loan) => (
-            <Loan 
-            content={loan} 
-            openErrorSnackbar={openErrorSnackbar} 
-            setOpenErrorSnackbar={setOpenErrorSnackbar} 
-            handleUpdate={handleUpdate} 
-            />
-        ))}
+        <Box>
+                {
+                    loanList.length === 0 ? (
+                        <Box sx={{backgroundColor: mode === 'light' ? 
+                                themeHandler.LIGHT_MODE.secondary_color : 
+                                themeHandler.DARK_MODE.secondary_color,
+                            }}
+                        height={50}
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='center'
+                        borderRadius={2}
+                        margin={1}
+                    >
+                        No hay solicitudes
+                    </Box>
+                    ) : (
+                    <>
+                        {loanList && loanList.map((loan) => (
+                            <Loan 
+                            content={loan} 
+                            openErrorSnackbar={openErrorSnackbar} 
+                            setOpenErrorSnackbar={setOpenErrorSnackbar} 
+                            handleUpdate={handleUpdate} 
+                            />
+                        ))}
+                    </>
+                    )
+                }
             <Snackbar open={openErrorSnackbar} anchorOrigin={{vertical: 'top', horizontal: 'left'}} autoHideDuration={3000} onClose={() => setOpenErrorSnackbar(false)}>
                 <Alert variant='filled' onClose={() => setOpenErrorSnackbar(false)} severity="error" sx={{ width: '100%' }}>
                     No se pudo modificar
